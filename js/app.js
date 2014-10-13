daf2db6f4723dee632ab2a4de7b3e74fad9eec1b
Drew Prescott <code@doprogress.com>
2014-10-12 20:45:47 -0500

    started new project

---
+++ js/app.js
@@ -0,0 +1,112 @@
+//Create the app
+App = Ember.Application.create(
+);
+
+
+//Create the routes for the app, the pages that will be accessed
+App.Router.map(function(){
+    this.route('about', {path: '/aboutus'}); //All main pages - not Nouns
+    this.resource('people', {path: '/family'}); //All type pages - Nouns
+    this.resource('products', function (){
+      this.resource('product', {path: '/:people_id'});
+    });
+    this.resource('jobs', function(){
+      this.resource('job', {path: '/:company'});
+    });
+});
+
+
+App.IndexController = Ember.Controller.extend({
+  productsCount: 6,
+  likes: function(){
+    return (33 + 3);
+  }.property(),
+  cat: 'img/cat.gif',
+  theTime: function(){
+    return (new Date()).toDateString()
+  }.property()
+});
+
+//Route for the page that will have a list of all the jobs.
+App.JobsRoute = Ember.Route.extend({
+  model: function(){
+    return App.COMPANIES;
+  }
+});
+
+//route that when called upon will have the data for the specific page.
+App.JobRoute = Ember.Route.extend({
+  model: function(params){
+    return App.COMPANIES.findBy('name', params.name);
+  }
+});
+
+//Route - how the data should be presented - access data
+//Fetches Data
+//Decides on what model to use for thecurrent route
+//decides which template to render to the screen
+App.PeopleRoute = Ember.Route.extend({
+  model: function(){
+    return App.PEOPLE;
+  }
+});
+
+App.ProductsRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.findAll('people');
+  }
+});
+
+App.ProductRoute = Ember.Route.extend({
+  model: function(params){
+    return this.store.find('people', params.people_id);
+  }
+});
+
+App.Product = DS.Model.extend({
+  name: DS.attr('string'),
+  age: DS.attr('number'),
+  family: DS.attr('boolean')
+
+});
+
+
+
+//Data for the Ember App
+App.People.FIXTURES = [
+  { id: 1,
+    name: 'jeremy',
+    age: 26,
+    family: true
+  },
+  { id: 2,
+    name: 'drew',
+    age: 27,
+    family: true
+  },
+  { id: 3,
+    name: 'bower',
+    age: 27,
+    family: false
+  },
+  { id: 4,
+    name: 'landon',
+    age: 1.3,
+    family: true
+  }
+];
+
+App.COMPANIES = [
+  {
+    company: 'Walmart',
+    reason: 'large company with great growth futuer prospects.'
+  },
+  {
+    company: 'Target',
+    reason: 'They are Walmarts largest competatior and they offer a great product.'
+  },
+  {
+    company: 'JC Penny',
+    reason: 'They have a long and great history and a potential future.'
+  }
+];
